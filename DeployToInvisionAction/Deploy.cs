68bcb299d827decf7ccb6b0ad948261a1730d9c9
Alex Redding <Alexredding180@gmail.com>
2021-01-14 17:43:03 +0000

    Update

--- DeployToInvisionAction/Deploy.cs
+++ DeployToInvisionAction/Deploy.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using DotNetEnv;
@@ -30,18 +32,29 @@ namespace DeployToInvisionAction
                 .CreateLogger();
         }
 
-        static bool UploadNewVersion()
+        private static bool UploadNewVersion()
         {
             var client = new RestClient(Env.GetString("ips_url"));
             var request =
-                new RestRequest($"api/downloads/files/{Env.GetInt("resource_id")}/history") {Method = Method.POST};
-            request.AddParameter("key", Env.GetString("api_key"), ParameterType.QueryString);
-            request.AddParameter($"linked_files[{Env.GetString("file_name")}]", Env.GetString("file_url"));
-            request.AddParameter("version", Env.GetString("version"));
-            request.AddParameter("changelog", Env.GetString("changelog"));
-            request.AddParameter("save", Env.GetBool("save_previous"));
+                new RestRequest($"api/downloads/files/{Env.GetInt("RESOURCE_ID")}/history") {Method = Method.POST};
+            request.AddParameter("key", Env.GetString("API_KEY"), ParameterType.QueryString);
+            request.AddParameter($"linked_files[{Env.GetString("FILE_NAME")}]", Env.GetString("FILE_URL"));
+            var version = Env.GetString("VERSION");
+            request.AddParameter("version", version);
+            if (!string.IsNullOrEmpty(Env.GetString("CHANGELOG_FILE")))
+            {
+                request.AddParameter("changelog", File.ReadAllLines(Env.GetString("CHANGELOG_FILE")));
+            }
+            else
+            {
+                request.AddParameter("changelog", Env.GetString("CHANGELOG"));
+            }
+            request.AddOrUpdateParameter("save", true);
+            if (GetResourceVersions(client)[0].Version == version)
+            {
+                request.AddOrUpdateParameter("save", false);
+            }
 
-            Log.Information(client.BuildUri(request).ToString());
             var restResponse = client.Execute(request);
             if (restResponse.StatusCode == HttpStatusCode.OK)
             {
@@ -49,6 +62,16 @@ namespace DeployToInvisionAction
             }
             Log.Error(restResponse.Content);
             return false;
+        }
+
+        private static List<ResourceVersion> GetResourceVersions(IRestClient client)
+        {
+            var request =
+                new RestRequest($"api/downloads/files/{Env.GetInt("RESOURCE_ID")}/history") {Method = Method.GET};
+            request.AddParameter("key", Env.GetString("API_KEY"), ParameterType.QueryString);
+
+            var restResponse = client.Execute(request);
+            return JsonConvert.DeserializeObject<List<ResourceVersion>>(restResponse.Content);
         }
     }
 }
---
+++ DeployToInvisionAction/ResourceVersion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace DeployToInvisionAction
+{
+    public class ResourceVersion
+    {
+        [JsonProperty("id")] public int Id { get; set; }
+
+        [JsonProperty("version")] public string Version { get; set; }
+
+        [JsonProperty("changelog")] public string Changelog { get; set; }
+
+        [JsonProperty("hidden")] public bool Hidden { get; set; }
+
+        [JsonProperty("date")] public DateTime Date { get; set; }
+    }
+}
